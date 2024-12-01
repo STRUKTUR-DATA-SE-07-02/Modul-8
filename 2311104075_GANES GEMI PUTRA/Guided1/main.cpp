6d924461278b7b1bc9ea764938c0bb942232f958
GANES2 <168392728+GANES2@users.noreply.github.com>
2024-12-01 22:21:08 +0700

    Add files via upload

--- 2311104075_GANES GEMI PUTRA/Guided1/main.cpp
+++ 2311104075_GANES GEMI PUTRA/Guided1/main.cpp
@@ -1,30 +1,93 @@
-#include <iostream>
+#include<iostream>
+
 using namespace std;
 
-struct mahasiswa {
-    char nim[10];
-    int nilai1, nilai2;
-};
+const int maksimalQueue = 5; // Maksimal antrian
+int front = 0; // Penanda antrian
+int back = 0; // Penanda
+string queueTeller[5]; // Fungsi pengecekan
 
-void inputMhs(mahasiswa &m);
-float rata2(mahasiswa m);
+bool isFull() { // Pengecekan antrian penuh atau tidak
+if (back == maksimalQueue) { return true; // =1
+} else {
+return false;
+}
+}
+
+bool isEmpty() { // Antriannya kosong atau tidak
+if (back == 0) { return true;
+} else {
+return false;
+}
+}
+
+void enqueueAntrian(string data) { // Fungsi menambahkan antrian
+if (isFull()) {
+cout << "Antrian penuh" << endl;
+} else {
+if (isEmpty()) { // Kondisi ketika queue kosong
+queueTeller[0] = data; front++;
+back++;
+} else { // Antrianya ada isi queueTeller[back] = data; back++;
+}
+}
+}
+
+void dequeueAntrian() { // Fungsi mengurangi antrian
+if (isEmpty()) {
+cout << "Antrian kosong" << endl;
+} else {
+for (int i = 0; i < back; i++) { queueTeller[i] = queueTeller[i + 1];
+}
+back--;
+}
+}
+
+int countQueue() { // Fungsi menghitung banyak antrian
+return back;
+}
+
+void clearQueue() { // Fungsi menghapus semua antrian
+if (isEmpty()) {
+cout << "Antrian kosong" << endl;
+} else {
+for (int i = 0; i < back; i++) { queueTeller[i] = "";
+}
+back = 0;
+front = 0;
+}
+}
+
+void viewQueue() { // Fungsi melihat antrian
+cout << "Data antrian teller:" << endl; for (int i = 0; i < maksimalQueue; i++) {
+if (queueTeller[i] != "") {
+cout << i + 1 << ". " << queueTeller[i] <<
+
+endl;
+
+
+} else {
+cout << i + 1 << ". (kosong)" << endl;
+
+}
+}
+}
 
 int main() {
-    mahasiswa mhs;
-    inputMhs(mhs);
-    cout << "Rata-rata nilai: " << rata2(mhs) << endl;
-    return 0;
-}
+enqueueAntrian("Andi");
 
-void inputMhs(mahasiswa &m) {
-    cout << "Input NIM: ";
-    cin >> m.nim;
-    cout << "Input nilai 1: ";
-    cin >> m.nilai1;
-    cout << "Input nilai 2: ";
-    cin >> m.nilai2;
-}
+enqueueAntrian("Maya");
 
-float rata2(mahasiswa m) {
-    return static_cast<float>(m.nilai1 + m.nilai2) / 2;
+viewQueue();
+cout << "Jumlah antrian = " << countQueue() << endl;
+
+dequeueAntrian();
+viewQueue();
+cout << "Jumlah antrian = " << countQueue() << endl;
+
+clearQueue();
+viewQueue();
+cout << "Jumlah antrian = " << countQueue() << endl;
+
+return 0;
 }
