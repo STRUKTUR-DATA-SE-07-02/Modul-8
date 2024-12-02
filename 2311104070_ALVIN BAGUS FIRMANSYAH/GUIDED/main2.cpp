8109228d9a6157f43e387e28f74e636ed621510e
AlvinBagusF <168392713+AlvinBagusF@users.noreply.github.com>
2024-12-02 20:34:19 +0700

    Add files via upload

---
+++ 2311104070_ALVIN BAGUS FIRMANSYAH/GUIDED/main2.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int value) {
+        data = value;
+        next = nullptr;
+    }
+};
+
+class Queue {
+private:
+    Node* front;
+    Node* rear;
+
+public:
+    Queue() {
+        front = rear = nullptr;
+    }
+
+    bool isEmpty() {
+        return front == nullptr;
+    }
+
+    void enqueue(int x) {
+        Node* newNode = new Node(x);
+        if (isEmpty()) {
+            front = rear = newNode;
+            return;
+        }
+        rear->next = newNode;
+        rear = newNode;
+    }
+
+    void dequeue() {
+        if (isEmpty()) {
+           cout << "Queue Underflow\n";
+            return;
+        }
+        Node* temp = front;
+        front = front->next;
+        delete temp;
+        if (front == nullptr)
+            rear = nullptr;
+    }
+
+    int peek() {
+        if (!isEmpty()) {
+            return front->data;
+        }
+       cout << "Queue is empty\n";
+        return -1;
+    }
+
+    void display() {
+        if (isEmpty()) {
+           cout << "Queue is empty\n";
+            return;
+        }
+        Node* current = front;
+        while (current) {
+           cout << current->data << " ";
+            current = current->next;
+        }
+       cout << "\n";
+    }
+};
+
+int main() {
+    Queue q;
+
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+
+   cout << "Queue elements: ";
+    q.display();
+
+   cout << "Front element: " << q.peek() << "\n";
+
+    q.dequeue();
+   cout << "After dequeuing, queue elements: ";
+    q.display();
+
+    return 0;
+}
